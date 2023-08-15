fcf805f8e4a87cd212d74874d6120630d8f1b6b8
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-08-14 21:18:46 -0300

    Ajustes Form Estabelecimento

--- client/src/components/PesquisaCEP/pesquisacep.vue
+++ client/src/components/PesquisaCEP/pesquisacep.vue
@@ -1,106 +1,231 @@
 <template>
+  <div id="form-wrap" class="form">
+    <br />
+    <h2>Crie seu Estabelecimento</h2>
+    <div class="posicaoform-wrap">
+      <div class="group">
+        <label for="nome" class="label">Nome do Estabelecimento:</label><br />
+        <input type="text" id="nome" class="input" name="nome" required /><br />
+      </div>
 
-<div >
-  <label class="label">Cep:
-    <input v-model="cep" class="input" type="text" id="cep" size="10" maxlength="9" @blur="pesquisarCEP" />
-  </label><br />
-  <label>Rua:
-    <input v-model="logradouro" type="text" id="rua" size="60" />
-  </label><br />
-  <label>Número:
-    <input v-model="numero" type="text"  size="60" />
-  </label><br />
-  <label>Bairro:
-    <input v-model="bairro" type="text" id="bairro" size="40" />
-  </label><br />
-  <label>Cidade:
-    <input v-model="cidade" type="text" id="cidade" size="40" />
-  </label><br />
-  <label>Estado:
-    <input v-model="uf" type="text" id="uf" size="2" />
-  </label><br />
-</div>
+      <div class="group">
+        <label for="cnpj" class="label">CNPJ:</label><br />
+        <input type="text" id="cnpj" class="input" name="cnpj" required /><br />
+      </div>
+      <!--Componente que faz a pesquisa do CEP -->
+      <PesquisaCEP />
+
+      <div class="group">
+        <label for="descricao" class="label"
+          >Descrição do Estabelecimento:</label
+        ><br />
+        <textarea
+          id="descricao"
+          class="input"
+          name="descricao"
+          rows="4"
+          maxlength="200"
+          required
+        ></textarea>
+        <br />
+        <span id="contador">0</span>/<span>200</span><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="tipo" class="label">Tipo do Anúncio:</label><br />
+        <input
+          type="radio"
+          id="doacao"
+          name="tipo"
+          value="doacao"
+          required
+          checked/>
+        <label for="doacao" class="label2">Doação</label><br />
+        <input type="radio" id="troca" name="tipo" value="troca" required />
+        <label for="troca" class="label2">Troca</label><br /><br />
+      </div>
 
 
+
+      <div class="group">
+        <label for="categoria" class="label">Categoria do Estabelecimento:</label><br />
+        <select id="categoria" class="input" name="categoria" required>
+          <option value="brinquedo">Bar</option>
+          <option value="roupa">Lanchonete</option>
+          <option value="alimento">Pizzaria</option>
+          <option value="carrinho">Hamburgueria</option></select
+        ><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="imagens" class="label">Imagens (Max. 4):</label><br />
+        <input
+          type="file"
+          id="imagens"
+          name="imagens[]"
+          accept="image/jpeg, image/gif, image/png, image/tiff, image/bmp"
+          multiple
+        /><br /><br />
+
+        <div id="miniatura-container">
+          <div
+            v-for="(imagem, index) in imagensSelecionadas"
+            :key="index"
+            class="miniatura"
+          >
+            <img :src="imagem.src" alt="Imagem" />
+            <button class="excluir" @click="excluirMiniatura(index)">
+              <i class="uil uil-times"></i>
+            </button>
+          </div>
+        </div>
+        <br /><br />
+      </div>
+
+      <div class="group">
+        <div>
+          <input type="button" class="button" value="Salvar" />
+          <input
+            type="button"
+            class="button"
+            value="Cancelar"
+            @click="cancelar"
+          />
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import IMask from "imask";
+import PesquisaCEP from '../PesquisaCEP/pesquisacep.vue';
+
 export default {
+  components: {
+    PesquisaCEP
+  },
+  name: "AddEstabelecimento",
   data() {
     return {
-      cep: '',
-      logradouro: '',
-      bairro: '',
-      cidade: '',
-      uf: ''
+      imagensSelecionadas: [], // Lista de imagens selecionadas
     };
   },
   methods: {
-    limparFormulario() {
-      this.logradouro = '';
-      this.bairro = '';
-      this.cidade = '';
-      this.uf = '';
+    excluirMiniatura(index) {
+      this.imagensSelecionadas.splice(index, 1);
     },
-    meuCallback(conteudo) {
-      if (!("erro" in conteudo)) {
-        this.logradouro = conteudo.logradouro;
-        this.bairro = conteudo.bairro;
-        this.cidade = conteudo.localidade;
-        this.uf = conteudo.uf;
-      } else {
-        this.limparFormulario();
-        alert("CEP não encontrado.");
-      }
+    cancelar() {
+      this.$router.push("/form-wrap");
     },
-    pesquisarCEP() {
-      const cep = this.cep.replace(/\D/g, '');
+  },
+  mounted() {
+    const cnpjInput = document.getElementById("cnpj");
+    const cnpjMask = IMask(cnpjInput, {
+      mask: "00.000.000/0000-00",
+    });
 
-      if (cep !== "") {
-        const validacep = /^[0-9]{8}$/;
+    const imagensInput = document.getElementById("imagens");
 
-        if (validacep.test(cep)) {
-          this.limparFormulario();
+    imagensInput.addEventListener("change", () => {
+      const files = Array.from(imagensInput.files);
+      const imagensRestantes = 4 - this.imagensSelecionadas.length;
+      const novasImagens = files.slice(0, imagensRestantes);
 
-          const script = document.createElement('script');
-          script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meuCallback`;
-          document.body.appendChild(script);
-          window.meuCallback = this.meuCallback;
-        } else {
-          this.limparFormulario();
-          alert("Formato de CEP inválido.");
-        }
-      } else {
-        this.limparFormulario();
-      }
-    }
-  }
+      novasImagens.forEach((file) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          const miniatura = { src: reader.result };
+          this.imagensSelecionadas.push(miniatura);
+        };
+
+        reader.readAsDataURL(file);
+      });
+
+      imagensInput.value = "";
+    });
+  },
 };
 </script>
 
 <style scoped>
-  label,
-  input {
+#form-wrap {
+  /*Imagem de fundo do forms*/
+    min-height: 1500px;
+    background-image: url('https://img.freepik.com/vetores-gratis/papel-de-parede-mural-do-restaurante_23-2148695092.jpg?w=900&t=st=1691866632~exp=1691867232~hmac=21d65c3cc870912f929637ba1f0b0e7e37fc93f5ad62c92f9803f5cf52f03be5');
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed; /* Aqui está o ponto chave */
     width: 100%;
-    color: #000;
+    height: 100vh;
+}
+
+span{
+  color: #fff;
+}
+
+.form .group .label2{
+  color: #fff;
+}
+
+.miniatura {
+  display: inline-block;
+  position: relative;
+  margin-right: 10px;
+}
+
+.miniatura img {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border: 3px solid #fff;
+}
+
+.miniatura .excluir {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  background-color: white;
+  border: none;
+  color: red;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+h2{
+  color: #fff;
+}
+
+.label {
+  color: #fff;
+}
+/* Responsividade */
+
+@media (max-width: 1160px) {
+  #form-wrap {
+    max-width: 950px;
+  }
+}
+
+@media (max-width: 950px) {
+  #form-wrap {
+    max-width: 768px;
+  }
+}
+
+@media (max-width: 768px) {
+  .group .button,
+  .group .input {
+    padding: 10px;
+    margin-bottom: 10px;
     display: block;
-     box-sizing: border-box;
+    width: 100%;
   }
 
-
- input {
-    border: none;
-    padding: 15px 100px;
-    border-radius: 25px;
-    background: rgba(211, 201, 201, 0.774);
+  .miniatura {
+    margin-bottom: 10px;
   }
-
-label {
-    text-transform: uppercase;
-  }
-
-label {
-    color: #fff;
-    font-size: 16px;
-  }
+}
 </style>
