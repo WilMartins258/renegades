ef5b1b456defcf84dd9922f9afed76a27b2af070
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-08-15 13:21:54 -0300

    teste

--- client/src/components/Estabelecimento/AddEstabelecimento.vue
+++ client/src/components/Estabelecimento/AddEstabelecimento.vue
@@ -1,20 +1,8 @@
 <template>
   <div id="form-wrap" class="form">
-    <br />
-    <h2>Crie seu Estabelecimento</h2>
     <div class="posicaoform-wrap">
-      <div class="group">
-        <label for="nome" class="label">Nome do Estabelecimento:</label><br />
-        <input type="text" id="nome" class="input" name="nome" required /><br />
-      </div>
-
-      <div class="group">
-        <label for="cnpj" class="label">CNPJ:</label><br />
-        <input type="text" id="cnpj" class="input" name="cnpj" required /><br />
-      </div>
-
       <!--Pesquisa por cep e completa os campos-->
-
+      <!--1-Endereço-->
       <div class="group">
         <label for="cep" class="label">CEP:</label><br />
         <input
@@ -79,6 +67,62 @@
         /><br />
       </div>
 
+      <!--2-Contato-->
+
+      <div class="group">
+        <label for="nome" class="label">Telefone:</label><br />
+        <input
+          type="text"
+          id="telefone"
+          class="input"
+          name="telefone"
+          required
+        /><br />
+      </div>
+
+      <div class="group">
+        <label for="whatsapp" class="label">É WhatsApp?</label><br />
+        <input type="checkbox" id="whatsapp" name="whatsapp" />
+      </div>
+
+      <div class="group">
+        <label for="nome" class="label">Celular:</label><br />
+        <input
+          type="text"
+          id="celular"
+          class="input"
+          name="celular"
+          required
+        /><br />
+      </div>
+
+      <div class="group">
+        <label for="whatsapp" class="label">É WhatsApp?</label><br />
+        <input type="checkbox" id="whatsapp" name="whatsapp" />
+      </div>
+
+      <div class="group">
+        <label for="nome" class="label">Informe seu site:</label><br />
+        <input type="text" id="site" class="input" name="site" /><br />
+      </div>
+
+      <div class="group">
+        <label for="nome" class="label">Cardápio Online:</label><br />
+        <input type="text" id="nome" class="input" name="nome" /><br />
+      </div>
+
+      <!--3-Dados do Estabelecimento-->
+
+      <div class="group">
+        <label for="nome" class="label">Nome do Estabelecimento:</label><br />
+        <input type="text" id="nome" class="input" name="nome" /><br />
+      </div>
+
+      <div class="group">
+        <label for="cnpj" class="label">CNPJ:</label><br />
+        <input type="text" id="cnpj" class="input" name="cnpj" required /><br />
+      </div>
+
       <div class="group">
         <label for="descricao" class="label"
           >Descrição do Estabelecimento:</label
@@ -96,34 +140,241 @@
       </div>
 
       <div class="group">
-        <label for="tipo" class="label">Tipo do Anúncio:</label><br />
+        <label for="tipo" class="label">Oferece Rodízio?</label><br />
         <input
           type="radio"
-          id="doacao"
+          id="rodizioSim"
           name="tipo"
-          value="doacao"
+          value="rodizioSim"
           required
           checked
         />
-        <label for="doacao" class="label2">Doação</label><br />
-        <input type="radio" id="troca" name="tipo" value="troca" required />
-        <label for="troca" class="label2">Troca</label><br /><br />
+        <label for="rodizioSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="rodizioNao"
+          name="tipo"
+          value="rodizioNao"
+          required
+        />
+        <label for="rodizioNao" class="label2">Não</label><br /><br />
       </div>
 
       <div class="group">
-        <label for="categoria" class="label"
-          >Categoria do Estabelecimento:</label
-        ><br />
-        <select id="categoria" class="input" name="categoria" required>
-          <option value="brinquedo">Bar</option>
-          <option value="roupa">Lanchonete</option>
-          <option value="alimento">Pizzaria</option>
-          <option value="carrinho">Hamburgueria</option></select
+        <label for="tipo" class="label">Precisa Agendar?</label><br />
+        <input
+          type="radio"
+          id="agendarSim"
+          name="tipo"
+          value="agendarSim"
+          required
+          checked
+        />
+        <label for="agendarSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="agendarNao"
+          name="tipo"
+          value="agendarNao"
+          required
+        />
+        <label for="agendarNao" class="label2">Não</label><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="tipo" class="label">Tem Estacionamento?</label><br />
+        <input
+          type="radio"
+          id="EstacionamentoSim"
+          name="tipo"
+          value="EstacionamentoSim"
+          required
+          checked
+        />
+        <label for="EstacionamentoSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="EstacionamentoNao"
+          name="tipo"
+          value="EstacionamentoNao"
+          required
+        />
+        <label for="EstacionamentoNao" class="label2">Não</label><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="tipo" class="label">Tem Estacionamento?</label><br />
+        <input
+          type="radio"
+          id="EstacionamentoSim"
+          name="tipo"
+          value="EstacionamentoSim"
+          required
+          checked
+        />
+        <label for="EstacionamentoSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="EstacionamentoNao"
+          name="tipo"
+          value="EstacionamentoNao"
+          required
+        />
+        <label for="EstacionamentoNao" class="label2">Não</label><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="tipo" class="label">Toca Musica?</label><br />
+        <input
+          type="radio"
+          id="musicaSim"
+          name="tipo"
+          value="musicaSim"
+          required
+          checked
+        />
+        <label for="musicaSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="musicaNao"
+          name="tipo"
+          value="musicaNao"
+          required
+        />
+        <label for="musicaNao" class="label2">Não</label><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="tipo" class="label">é ao Musica?</label><br />
+        <input
+          type="radio"
+          id="aoVivoSim"
+          name="tipo"
+          value="aoVivoSim"
+          required
+          checked
+        />
+        <label for="musicaSim" class="label2">Sim</label><br />
+        <input
+          type="radio"
+          id="aoVivoNao"
+          name="tipo"
+          value="aoVivoNao"
+          required
+        />
+        <label for="aoVivoNao" class="label2">Não</label><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="generoMusica" class="label">Gênero de Música:</label><br />
+        <select id="generoMusica" class="input" name="generoMusica" required>
+          <option value="rock">Rock</option>
+          <option value="sertanejo">Sertanejo</option>
+          <option value="jazz">Jazz</option>
+          <option value="pop">Pop</option></select
         ><br /><br />
       </div>
 
       <div class="group">
-        <label for="imagens" class="label">Imagens (Max. 4):</label><br />
+        <label for="categoriaEstabelecimento" class="label"
+          >Categoria do Estabelecimento:</label
+        ><br />
+        <select
+          id="categoriaEstabelecimento"
+          class="input"
+          name="categoriaEstabelecimento"
+          required
+        >
+          <option value="bar">Bar</option>
+          <option value="lanchonete">Lanchonete</option>
+          <option value="pizzaria">Pizzaria</option>
+          <option value="hamburgueria">Hamburgueria</option></select
+        ><br /><br />
+      </div>
+
+      <!--3-Prato Principal-->
+
+      <h2>Cadastre até 3 pratos pricipais</h2>
+
+      <div class="group">
+        <label for="nomePrato1" class="label">Item 1:</label><br />
+        <input
+          type="text"
+          id="nomePrato1"
+          class="input"
+          name="nomePrato1"
+        /><br />
+      </div>
+
+      <div class="group">
+        <label for="descricaoItem1" class="label">Descrição do item 1:</label
+        ><br />
+        <textarea
+          id="descricaoItem1"
+          class="input"
+          name="descricaoItem1"
+          rows="4"
+          maxlength="200"
+          required
+        ></textarea>
+        <br />
+        <span id="contador">0</span>/<span>200</span><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="nomePrato2" class="label">Item 2:</label><br />
+        <input
+          type="text"
+          id="nomePrato2"
+          class="input"
+          name="nomePrato2"
+        /><br />
+      </div>
+
+      <div class="group">
+        <label for="descricaoItem2" class="label">Descrição do item 1:</label
+        ><br />
+        <textarea
+          id="descricaoItem2"
+          class="input"
+          name="descricaoItem2"
+          rows="4"
+          maxlength="200"
+          required
+        ></textarea>
+        <br />
+        <span id="contador">0</span>/<span>200</span><br /><br />
+      </div>
+
+      <div class="group">
+        <label for="nomePrato3" class="label">Item 3:</label><br />
+        <input
+          type="text"
+          id="nomePrato3"
+          class="input"
+          name="nomePrato3"
+        /><br />
+      </div>
+
+      <div class="group">
+        <label for="descricaoItem3" class="label">Descrição do item 3:</label
+        ><br />
+        <textarea
+          id="descricaoItem3"
+          class="input"
+          name="descricaoItem3"
+          rows="4"
+          maxlength="200"
+          required
+        ></textarea>
+        <br />
+        <span id="contador">0</span>/<span>200</span><br /><br />
+      </div>
+
+      <!--foto prato principal-->
+
+      <div class="group">
+        <label for="imagens" class="label">Imagens (Max. 3):</label><br />
         <input
           type="file"
           id="imagens"
@@ -140,6 +391,34 @@
           >
             <img :src="imagem.src" alt="Imagem" />
             <button class="excluir" @click="excluirMiniatura(index)">
+              <i class="uil uil-times"></i>
+            </button>
+          </div>
+        </div>
+        <br /><br />
+      </div>
+
+      <!--foto Estabelecimento-->
+
+      <div class="group">
+        <label for="imagemEstabelecimento" class="label"
+          >Imagem do Estabelecimento:</label
+        ><br />
+        <input
+          type="file"
+          id="imagemEstabelecimento"
+          name="imagemEstabelecimento"
+          accept="image/jpeg, image/gif, image/png, image/tiff, image/bmp"
+          @change="adicionarImagemEstabelecimento"
+        /><br /><br />
+
+        <div id="miniatura-imagem-estabelecimento">
+          <div class="miniatura">
+            <img
+              :src="imagemEstabelecimentoSrc"
+              alt="Imagem do Estabelecimento"
+            />
+            <button class="excluir" @click="excluirImagemEstabelecimento">
               <i class="uil uil-times"></i>
             </button>
           </div>
@@ -171,6 +450,7 @@ export default {
   data() {
     return {
       imagensSelecionadas: [],
+      imagemEstabelecimentoSrc: "",
       cnpj: "",
       cep: "",
       endereco: {
@@ -228,6 +508,21 @@ export default {
         this.limpa_formulário_cep();
       }
     },
+    adicionarImagemEstabelecimento(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.imagemEstabelecimentoSrc = reader.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+    excluirImagemEstabelecimento() {
+      this.imagemEstabelecimentoSrc = "";
+    },
+
+
   },
   mounted() {
     const cnpjInput = document.getElementById("cnpj");
@@ -235,11 +530,22 @@ export default {
       mask: "00.000.000/0000-00",
     });
 
+    const telefoneInput = document.getElementById("telefone");
+    const telefoneMask = IMask(telefoneInput, {
+      mask: "(00) 0000-0000",
+    });
+
+    const celularInput = document.getElementById("celular");
+    const celularMask = IMask(celularInput, {
+      mask: "(00) 00000-0000",
+    });
+
+    /*Img Cardápio*/
     const imagensInput = document.getElementById("imagens");
 
     imagensInput.addEventListener("change", () => {
       const files = Array.from(imagensInput.files);
-      const imagensRestantes = 4 - this.imagensSelecionadas.length;
+      const imagensRestantes = 3 - this.imagensSelecionadas.length;
       const novasImagens = files.slice(0, imagensRestantes);
 
       novasImagens.forEach((file) => {
@@ -255,6 +561,10 @@ export default {
 
       imagensInput.value = "";
     });
+
+    /*Img Logo Estabelecimento*/
+
+    
   },
 };
 </script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,85 @@
 <template>
   <div class="main-container">
-    <h1>Home</h1>
+    <div>
+    <div v-if="currentStep === 1">
+      <!-- Seção 1: Endereço -->
+      <h2>Seção 1: Endereço</h2>
+      <!-- Inputs do endereço -->
+      <input v-model="address.street" placeholder="Rua">
+      <input v-model="address.city" placeholder="Cidade">
+      <button @click="nextStep">Próximo</button>
+    </div>
+
+    <div v-else-if="currentStep === 2">
+      <!-- Seção 2: Contatos -->
+      <h2>Seção 2: Contatos</h2>
+      <!-- Inputs de contatos -->
+      <input v-model="contacts.email" placeholder="Email">
+      <input v-model="contacts.phone" placeholder="Telefone">
+      <button @click="prevStep">Anterior</button>
+      <button @click="nextStep">Próximo</button>
+    </div>
+
+    <div v-else-if="currentStep === 3">
+      <!-- Seção 3: Dados Pessoais -->
+      <h2>Seção 3: Dados Pessoais</h2>
+      <!-- Inputs de dados pessoais -->
+      <input v-model="personalData.name" placeholder="Nome">
+      <input v-model="personalData.age" placeholder="Idade">
+      <button @click="prevStep">Anterior</button>
+      <button @click="nextStep">Próximo</button>
+    </div>
+
+    <div v-else>
+      <!-- Seção 4: Definido -->
+      <h2>Seção 4: Definido</h2>
+      <!-- Conteúdo da seção 4 -->
+      <button @click="prevStep">Anterior</button>
+      <button @click="saveForm">Salvar</button>
+      <button @click="cancelForm">Cancelar</button>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-  
+  export default {
+  data() {
+    return {
+      currentStep: 1,
+      address: {
+        street: '',
+        city: ''
+      },
+      contacts: {
+        email: '',
+        phone: ''
+      },
+      personalData: {
+        name: '',
+        age: ''
+      }
+    };
+  },
+  methods: {
+    nextStep() {
+      if (this.currentStep < 4) {
+        this.currentStep++;
+      }
+    },
+    prevStep() {
+      if (this.currentStep > 1) {
+        this.currentStep--;
+      }
+    },
+    saveForm() {
+      // Lógica para salvar os dados do formulário
+    },
+    cancelForm() {
+      // Lógica para cancelar o formulário
+    }
+  }
+};
 </script>
 
 <style scoped>
