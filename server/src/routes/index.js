36bcc066f0467ab1a921ce8c1e9755c4dea59a47
WilMartins258 <willmartins258@gmail.com>
2023-08-15 15:14:12 -0300

    ajuste na rota home

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,15 +6,10 @@ const cors = require('cors');
 router.use(express.json());
 router.use(cors());
 
-router.get('/', (req, res) => {
-    console.log('passou pelo /')
-    res.send('Dados da página inicial Renagades.');
-});
-
 // Rotas do backend
 
 router.use(
-    "/home",
+    "/",
     require("./home.js")
 );
 
