11594a1754cc37715e9580974743e0d74c67621f
WilMartins258 <willmartins258@gmail.com>
2023-08-22 10:24:48 -0300

    feat: adicionando configuração de IP público para rotas

--- client/src/backend-service.js
+++ client/src/backend-service.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-/**
- * Futuramente podemos e devemos usar ENV (variável de ambiente) para configurar isso aqui.
- */
+// Ao mater o localhost:PORTA vai direcionar as requisições para o ambiente local
 const baseURL = "http://localhost:3001/";
+
+// Ao comentar a linha acima e descomentar a linha abaixo trocando "MEU_IP_PUBLICO" pelo IP desejado irá direcionar as requisições para este IP
+// const baseURL = "http://MEU_IP_PUBLICO:3001/";
 
 const api = axios.create({
   baseURL
