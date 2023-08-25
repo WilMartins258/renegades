e922ce61fdc08455021ee0d20a73158330deaaf2
WilMartins258 <willmartins258@gmail.com>
2023-08-25 10:56:50 -0300

    criacao da rota de categorias

--- server/src/routes/categorias.js
+++ server/src/routes/categorias.js
@@ -1,7 +1,25 @@
 // Importando módulos
-const express = require('express');
 const router = require('express').Router();
 const db = require('../db');
+
+    const getAllCategorias = async () => {
+        const categoriasQuery = "SELECT * FROM categoria";
+        const connection = await db;
+
+        const [categorias] = await connection.query(categoriasQuery);
+    
+        return categorias;
+    }
+
+router.get('/', async (req, res) => {
+
+    // O resultado disso é um array com cada categoria em um elemento
+    const categorias = await getAllCategorias();
+    
+    res.status(200).send(
+        categorias
+    );
+});
 
 
 module.exports = router;
