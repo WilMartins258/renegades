063d6840f7da56d174e9b51c4a789bba7787c790
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-08-27 14:19:51 -0300

    Dash Horaio de Atendimento v1

--- client/src/components/Home/TelaHome.vue
+++ client/src/components/Home/TelaHome.vue
@@ -3,17 +3,22 @@
       <h3>Mais Recomendados!!</h3>
       <CombinedCarousel/><br><br>
       <Filtro/>
+      <br><br><h1>.</h1><br><br>
+      <DashHorAtendimento/>
+     
   
   </template>
   
   <script>
   import CombinedCarousel from "../Home/ComponentsChild/Carousel.vue"
   import Filtro from "../Home/ComponentsChild/Filtro.vue"
+  import DashHorAtendimento from "../Estabelecimento/DashHorAtendimento.vue"
       export default{
           name: "TelaHome",
           components:{
             CombinedCarousel,
-            Filtro
+            Filtro,
+            DashHorAtendimento,
           }
       };
   
