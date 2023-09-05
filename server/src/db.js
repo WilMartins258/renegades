5bc02a510271296f26f9af1408438c54e977052f
WilMartins258 <willmartins258@gmail.com>
2023-09-05 20:56:14 -0300

    adicionando tratamento de erros ao arquivo de conexão com o banco de dados

--- server/src/db.js
+++ server/src/db.js
@@ -1,19 +1,22 @@
 /**
- * Esse arquivo tem o único intuito de estabelecer uma coneção com o banco de dados e disponibilizá-la para todo o projeto
- * @returns connection -> Retornar uma instância de conexão com o banco de dados
+ * Esse arquivo tem o único intuito de estabelecer uma conexão com o banco de dados e disponibilizá-la para todo o projeto
+ * @returns connection -> Retorna uma instância de conexão com o banco de dados
  */
-async function connect(){
-    if (global.connection && global.connection.state !== 'disconnected') {
-        return global.connection;
-    }
+async function connect() {
+    try {
+        if (global.connection && global.connection.state !== 'disconnected') {
+            return global.connection;
+        }
 
-    const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://root:123456@localhost:3306/renegades_stage");
-    console.log("Conectou com MySQL!");
-    global.connection = connection;
-    return connection;
+        const mysql = require("mysql2/promise");
+        const connection = await mysql.createConnection("mysql://root:123456@localhost:3306/renegades_stage");
+        console.log("Conectou com MySQL!");
+        global.connection = connection;
+        return connection;
+    } catch (error) {
+        console.error("Erro ao conectar com MySQL:", error);
+        throw new Error("Erro ao conectar com o banco de dados");
+    }
 }
 
 module.exports = connect();
-
-//  mysql://usuario:senha@servidor:porta/banco
