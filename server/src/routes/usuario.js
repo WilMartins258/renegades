9622129532b0dc82c8a826750d018589018048a2
WilMartins258 <willmartins258@gmail.com>
2023-09-05 19:01:06 -0300

    adicionando tratamento de erros na rota de usuário

--- server/src/routes/usuario.js
+++ server/src/routes/usuario.js
@@ -8,31 +8,35 @@ const separarCelularService = require('../services/separarCelular.service.js');
 const dataToMySqlService = require('../services/dataToMySql.service.js');
 
 router.get('/', async (req, res) => {
-    const reqBody = req.body;
-
-    // console.log('reqBody:: \n', req)
-    res.status(200).send(
-        'Adicione o ID do usuário desejado no final da rota, exemplo: usuario/id'
-    );
+    try {
+        const reqBody = req.body;
+        res.status(200).send(
+            'Adicione o ID do usuário desejado no final da rota, exemplo: usuario/id'
+        );
+    } catch (error) {
+        console.error('Erro na rota GET /: ', error);
+        res.status(500).send({
+            errorMsg: 'Ocorreu um erro ao processar a solicitação.',
+            error: error.message
+        });
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    // Acesse o valor do parâmetro "id" usando req.params.id
-    const userId = req.params.id;
-    // console.log('Início da rota usuario :id');
-
-    // Recebe os dados de um usuário passando como parâmetro seu ID
-    const dadosUsuario = await userController.getUserById(userId);
-
-    // console.log('Fim da rota usuario :id');
-    if (dadosUsuario) {
-        res.status(200).send(
-            dadosUsuario
-        );
-    } else {
-        res.status(404).send(
-            'Id de usuário não encontrado!'
-        );
+    try {
+        const userId = req.params.id;
+        const dadosUsuario = await userController.getUserById(userId);
+        if (dadosUsuario) {
+            res.status(200).send(dadosUsuario);
+        } else {
+            res.status(404).send('Id de usuário não encontrado!');
+        }
+    } catch (error) {
+        console.error('Erro na rota GET /:id', error);
+        res.status(500).send({
+            errorMsg: 'Ocorreu um erro ao processar a solicitação.',
+            error: error.message
+        });
     }
 });
 
@@ -49,7 +53,6 @@ router.put('/', async (req, res) => {
             sobrenome: nomeDividido.sobrenome,
             codigo: numeroDividido?.codigoArea,
             celular: numeroDividido?.numero,
-            // cpf: null, // Posteriormente ou iremos remover o CPF do banco ou iremos adicionar ele ao front
             email: reqBody.email,
             senha: reqBody.senha,
             foto: 'test-blob',
@@ -65,29 +68,29 @@ router.put('/', async (req, res) => {
             msg: 'Dados do usuário alterados com sucesso!'
         });
     } catch (error) {
-        console.error('ERROR:: ', error);
+        console.error('Erro na rota PUT /', error);
         res.status(500).send({
             errorMsg: 'Ocorreu um erro ao processar a solicitação.',
-            error: error
+            error: error.message
         });
     }
 });
 
 router.post('/', async (req, res) => {
-    console.log('Início da rota usuario POST');
-    const reqBody = req.body;
+    try {
+        console.log('Início da rota usuario POST');
+        const reqBody = req.body;
 
-    // console.log('reqBody:: \n', reqBody);
+        // Lógica para adicionar um usuário aqui
 
-    const dadosUsuario = {
-
-
-    };
-
-
-    res.status(200).send(
-        `Usuário adicionado ao sistema`
-    );
+        res.status(200).send(`Usuário adicionado ao sistema`);
+    } catch (error) {
+        console.error('Erro na rota POST /', error);
+        res.status(500).send({
+            errorMsg: 'Ocorreu um erro ao processar a solicitação.',
+            error: error.message
+        });
+    }
 });
 
 module.exports = router;
