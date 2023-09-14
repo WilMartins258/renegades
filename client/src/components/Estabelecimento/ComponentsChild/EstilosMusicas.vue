14db9ec631398e2beac72a18b3e5806d4d41bd04
WilMartins258 <willmartins258@gmail.com>
2023-09-14 20:49:15 -0300

    integração do back com o front no componente de estilos de música

--- client/src/components/Estabelecimento/ComponentsChild/EstilosMusicas.vue
+++ client/src/components/Estabelecimento/ComponentsChild/EstilosMusicas.vue
@@ -1,115 +1,105 @@
 <template>
-    <div class="quadro-estilosMusicais">
-      <button
-        v-for="opcao in estilos"
-        :key="opcao.id"
-        :class="{ botao: true, selecionado: opcaoSelecionada(opcao) }"
-        @click="selecionarOpcao(opcao)"
-      >
-        {{ opcao.nome }}
-      </button>
-    </div>
+  <div class="quadro-estilosMusicais">
+    <button v-for="opcao in estilos" :key="opcao.id" :class="{ botao: true, selecionado: opcaoSelecionada(opcao) }"
+      @click="selecionarOpcao(opcao)">
+      {{ opcao.nome }}
+    </button>
+  </div>
+
+  <div id="estilosSelecionadas">
+    <br>
+    <p>Estilos selecionados: {{ estilosSelecionadasString }}</p>
+  </div>
+</template>
   
-    <div id="estilosSelecionadas">
-      <br><p>Estilos selecionados: {{ estilosSelecionadasString }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "EstilosMusicas",
-    props: {
+<script>
+import api from './../../../services/backend-service.js';
+export default {
+
+  name: "EstilosMusicas",
+  props: {
     value: Array, // Adicione uma propriedade value para usar v-model
-    },
-    emits: ['input'],
-    data() {
-      return {
-        //estilos: [] // usar com a API
-        estilosSelecionadas: [],
-        estilos: [ // remover ou comentar ao testar a API
-          {
-            id: 1,
-            nome: "Rock"
-          },
-          {
-            id: 2,
-            nome: "Sertanejo"
-          },
-          {
-            id: 3,
-            nome: "Hip-hop"
-          },
-          {
-            id: 4,
-            nome: "Funk"
-          },
-          {
-            id: 5,
-            nome: "Clássica"
-          }
-        ]
-      };
-    },
-    computed: {
-      estilosSelecionadasString() {
-        // Converte o array de opções selecionadas em uma string separada por vírgula
-        return this.estilosSelecionadas.map(opcao => opcao.nome).join(", ");
-      }
-    },
-    methods: {
-      selecionarOpcao(opcao) {
-        if (this.opcaoSelecionada(opcao)) {
-          // Se a opção já estiver selecionada, remova-a da lista
-          this.estilosSelecionadas = this.estilosSelecionadas.filter(item => item.id !== opcao.id);
-        } else {
-          // Caso contrário, adicione-a à lista
-          this.estilosSelecionadas.push(opcao);
-        }
-        this.$emit("input", this.estilosSelecionadas); // Emita o evento input aqui
-      },
-      opcaoSelecionada(opcao) {
-        return this.estilosSelecionadas.some(item => item.id === opcao.id);
-      },
-      getestilos() {
-        // Fazer chamada a API para obter as informações das opções.
-      }
+  },
+  emits: ['input'],
+  data() {
+    return {
+      estilosSelecionadas: [],
+      estilos: []
+    };
+  },
+  computed: {
+    estilosSelecionadasString() {
+      // Converte o array de opções selecionadas em uma string separada por vírgula
+      return this.estilosSelecionadas.map(opcao => opcao.nome).join(", ");
     }
-  };
-  </script>
-  
-  <style scoped>
-  .quadro-estilosMusicais {
-    border: 2px solid white;
-    padding: 20px;
-    width: 300px;
-    text-align: center;
-    margin: 0 auto;
-    background-color: rgba(255, 255, 255, 0.418); 
-  }
+  },
+  created() {
+    this.metodoInicial();
+  },
+  methods: {
+    async metodoInicial() {
+      try {
+        const estilosMusica = await api.get('/estiloMusica');
 
-  p{ /*ajustar de acordo com o fundo*/
-    color: white;
+        this.estilos = estilosMusica.data;
+      } catch (error) {
+        console.error('ERROR:: ', error);
+      }
+    },
+    selecionarOpcao(opcao) {
+      if (this.opcaoSelecionada(opcao)) {
+        // Se a opção já estiver selecionada, remova-a da lista
+        this.estilosSelecionadas = this.estilosSelecionadas.filter(item => item.id !== opcao.id);
+      } else {
+        // Caso contrário, adicione-a à lista
+        this.estilosSelecionadas.push(opcao);
+      }
+      this.$emit("input", this.estilosSelecionadas); // Emita o evento input aqui
+    },
+    opcaoSelecionada(opcao) {
+      return this.estilosSelecionadas.some(item => item.id === opcao.id);
+    },
+    getestilos() {
+      // Fazer chamada a API para obter as informações das opções.
+    }
   }
+};
+</script>
   
-  .botao {
-    background-color: red;
-    color: white;
-    padding: 10px 20px;
-    margin: 5px;
-    cursor: pointer;
-    border-radius: 25px; 
-  }
-  
-  .botao.selecionado {
-    background-color: orange;
-  }
-  
-  .botao:hover {
-    background-color: yellow;
-  }
+<style scoped>
+.quadro-estilosMusicais {
+  border: 2px solid white;
+  padding: 20px;
+  width: 300px;
+  text-align: center;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.418);
+}
 
-  /*Responsivo */
-  @media screen and (max-width: 1160px) {
+p {
+  /*ajustar de acordo com o fundo*/
+  color: white;
+}
+
+.botao {
+  background-color: red;
+  color: white;
+  padding: 10px 20px;
+  margin: 5px;
+  cursor: pointer;
+  border-radius: 25px;
+}
+
+.botao.selecionado {
+  background-color: orange;
+}
+
+.botao:hover {
+  background-color: yellow;
+}
+
+/*Responsivo */
+@media screen and (max-width: 1160px) {
   .quadro-estilosMusicais {
     width: 100%;
   }
@@ -138,5 +128,5 @@
     width: 100%;
   }
 }
-  </style>
+</style>
   
