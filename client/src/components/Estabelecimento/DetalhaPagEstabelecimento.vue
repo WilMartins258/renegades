a48c015a13de29d9653ee18fea04e2b4a3753b64
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-09-15 21:58:21 -0300

    Pagina de apresentação do estabelecimento

--- client/src/components/Estabelecimento/DetalhaPagEstabelecimento.vue
+++ client/src/components/Estabelecimento/DetalhaPagEstabelecimento.vue
@@ -5,7 +5,7 @@
         <p>Pizzaria</p>
         <img src="imagem_do_estabelecimento.jpg" alt="Imagem do Estabelecimento">
         <div class="icons">
-            <span class="favorite-icon">Favoritar</span>
+            <span class=" star-icon" :class="{ 'selected': favorito }" @click="toggleFavorito">&#9733;</span>
             <span class="share-icon">Compartilhar</span>
         </div>
     </header>
@@ -15,26 +15,26 @@
         <p>Coloque aqui a descrição do estabelecimento.</p>
     </section>
 
-    <section class="chef-recommendations">
+    <section class="chef-recomendacoes">
         <!-- Quadros de recomendações do chef -->
-        <div class="recommendation">
+        <div class="recomendacao">
             <h3>Prato Principal 1</h3>
             <p>Descrição do prato principal 1.</p>
             <img src="imagem_prato1.jpg" alt="Imagem do Prato Principal 1">
         </div>
-        <div class="recommendation">
+        <div class="recomendacao">
             <h3>Prato Principal 2</h3>
             <p>Descrição do prato principal 2.</p>
             <img src="imagem_prato2.jpg" alt="Imagem do Prato Principal 2">
         </div>
-        <div class="recommendation">
+        <div class="recomendacao">
             <h3>Prato Principal 3</h3>
             <p>Descrição do prato principal 3.</p>
             <img src="imagem_prato3.jpg" alt="Imagem do Prato Principal 3">
         </div>
     </section>
 
-    <section class="additional-info">
+    <section class="adicionais-info">
         <!-- Quadros de informações adicionais -->
         <div class="info">
             <h3>O que oferecemos</h3>
@@ -50,37 +50,76 @@
         </div>
     </section>
     
-<section class="address-info">
-    <div class="address">
+<section class="endereco-info">
+    <div class="endereco">
     <h2>Endereço</h2>
     <p>Insira aqui o endereço do estabelecimento.</p>
     </div>
-    <div class="address">
+    <div class="endereco">
     <!-- Espaço para exibir o mapa (substitua a URL pelo código do mapa real) -->
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.349676015265!2dSUA-LONGITUDE!3dSUA-LATITUDE!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zSsKwMTcnMzcuNSJTIDQwwrAyMSc1LjciRQ!5e0!3m2!1spt-BR!2sbr!4v1618986094141!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 </section>
 
 	
-    <section class="user-rating">
+    <section class="user-avaliacao">
         <h2>Avalie</h2>
         <form>
-            <label for="rating">Avaliação:</label>
-            <input type="number" id="rating" name="rating" min="1" max="5" step="1">
+            <label for="avaliacao">De uma Nota:</label>
+            <div class="rating">
+                <span class="star" :class="{ 'selected': nota >= 1 }" @click="selecionarNota(1)" >&#9733;</span>
+                <span class="star" :class="{ 'selected': nota >= 2 }" @click="selecionarNota(2)" >&#9733;</span>
+                <span class="star" :class="{ 'selected': nota >= 3 }" @click="selecionarNota(3)" >&#9733;</span>
+                <span class="star" :class="{ 'selected': nota >= 4 }" @click="selecionarNota(4)" >&#9733;</span>
+                <span class="star" :class="{ 'selected': nota >= 5 }" @click="selecionarNota(5)" >&#9733;</span>
+            </div>
             <br>
             <label for="user-review">Digite sua avaliação:</label>
-            <textarea id="user-review" name="user-review" rows="4" cols="50"></textarea>
+            <div class="group">
+            <textarea id="user-review" class="input" name="user-review" rows="4" cols="50" ref="userReview"></textarea>
             <br>
-            <input type="submit" value="Validar">
+            
+            <div>
+                <input type="button" class="button" value="Avaliar" @click.prevent="avaliar"/>
+            </div>
+        </div>
         </form>
     </section>
-
+     
 </template>
 
 <script>
 export default {
     name: "DetalhaPagEstabelecimento",
+    data() {
+        return {
+            nota: 0, 
+            favorito: false,
+        };
+    },
+    methods: {
+        selecionarNota(notaSelecionada) {
+            // Atualiza a valor de nota
+            this.nota = notaSelecionada;
+        },
+        avaliar() {
+            // Pega a avaliação do usuário
+            const avaliacao = document.getElementById("user-review").value;
 
+            // teste de saida:
+            console.log(`Nota: ${this.nota}`);
+            console.log(`Avaliação: ${avaliacao}`);
+            console.log(`Favoritar: ${this.favorito}`);
+            this.limpa_Avaliacao();
+        },
+        limpa_Avaliacao() {
+            this.nota = 0; // Define a nota como 0
+            this.$refs.userReview.value = ""; // Limpa o campo de avaliação
+        },
+        toggleFavorito() {
+            this.favorito = !this.favorito;
+        },
+    }
 }
 
 </script>
@@ -88,7 +127,6 @@ export default {
 <style scoped>
 /* Estilos gerais */
 body {
-    font-family: Arial, sans-serif;
     margin: 0;
     padding: 0;
 }
@@ -122,13 +160,13 @@ header h1 {
 }
 
 /* Estilos das recomendações do chef */
-.chef-recommendations {
+.chef-recomendacoes {
     display: flex;
     justify-content: space-between;
     padding: 20px;
 }
 
-.recommendation {
+.recomendacao {
     flex: 1;
     padding: 10px;
     border: 1px solid #ccc;
@@ -139,7 +177,7 @@ header h1 {
 
 
 /* Estilos das informações adicionais */
-.additional-info {
+.adicionais-info {
     display: flex;
     justify-content: space-between;
     padding: 20px;
@@ -153,12 +191,12 @@ header h1 {
     margin: 0 10px;
 }
 
-.address-info{
+.endereco-info{
     display: flex;
     justify-content: space-between;
     padding: 20px;
 }
-.address{
+.endereco{
     flex: 1;
     padding: 10px;
     border: 1px solid #ccc;
@@ -166,22 +204,60 @@ header h1 {
     margin: 0 10px;
 }
 
+/* Estilos da seção de avaliação do usuário */
+.user-avaliacao {
+    padding: 20px;
+    background-color: #f0f0f0;
+    text-align: center;
+    max-width: 700px; 
+    margin: 0 auto; 
+}
+
+/* Estilos para o input de avaliação */
+.input {
+    width: 100%;
+    color: #000;
+    display: block;
+    box-sizing: border-box; /* Inclui padding e borda na largura total */
+    max-width: 90%; /* Largura máxima de 100% */
+    margin: 0 auto;
+}
 
 /* Estilos da seção de avaliação do usuário */
-.user-rating {
+.user-avaliacao {
     padding: 20px;
     background-color: #f0f0f0;
     text-align: center;
 }
 
-/* Estilos para os botões */
-input[type="submit"] {
-    background-color: #333;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    cursor: pointer;
+/* Estilos das Estrelas seção de avaliação do usuário*/
+.rating {
+    font-size: 24px;
+    text-align: center;
 }
+
+.star {
+    cursor: pointer;
+    color: gray;
+    transition: color 0.2s; /* Transição suave de cor ao passar o mouse */
+}
+
+.star.selected {
+    color: yellow;
+}
+
+.star-icon {
+    font-size: 26px; /* Tamanho da fonte desejado para a estrela */
+    cursor: pointer;
+    transition: color 0.2s; /* Transição suave de cor ao passar o mouse */
+}
+
+.star-icon.selected  {
+    color: yellow;
+}
+
+
+
 
 /* Estilos para links */
 a {
@@ -189,9 +265,53 @@ a {
     text-decoration: none;
 }
 
+
 a:hover {
     text-decoration: underline;
 }
 
+/* Estilos para os botões */
+.group {
+    margin-bottom: 15px;
+  }
+  
+ .group .label,
+.group .input {
+    width: 100%;
+    color: #000;
+    display: block;
+  }
+  
+  .group .button,
+  .group .input {
+    border: none;
+    padding: 15px 20px;
+    border-radius: 25px;
+    background: rgba(211, 201, 201, 0.774);
+  }
+  
+  .group .button
+  {
+    text-transform: uppercase;
+  }
+  
+  .group .button-spacing {
+    margin-right: 10px;
+  }
+  
+.group .button {
+    padding: 15px 30px;
+  }
+
+.group .button {
+    background:#e91e2f;
+    cursor: pointer;
+    transition: 0.5s;
+    color: #fff;
+  }
+  
+.group .button:hover {
+    background:#ff9800;
+  }
 
 </style>
