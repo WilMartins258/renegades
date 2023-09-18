201aaf56f1f33b87bce099893b117fd5b76792ca
WilMartins258 <willmartins258@gmail.com>
2023-09-18 18:53:28 -0300

    criando o createEndereco

--- server/src/services/endereco.service.js
+++ server/src/services/endereco.service.js
@@ -3,14 +3,27 @@ const db = require('../models/db.js');
 const getEnderecoById = async (enderecoId) => {
     const dadosEnderecoQuery = "SELECT * FROM endereco WHERE id = ?";
     const connection = await db;
-    
+
     const [dadosEndereco] = await connection.query(dadosEnderecoQuery, enderecoId);
 
     return dadosEndereco[0];
 };
 
+const createEndereco = async (enderecoData) => {
+    try {
+        const inserirEnderecoQuery = `INSERT INTO endereco VALUES ( null, ?, ?, ?, ?, ?, ? );`;
+        const connection = await db;
+
+        const [idEnderecoInserido] = await connection.query(inserirEnderecoQuery, enderecoData);
+
+        return idEnderecoInserido?.insertId;
+    } catch (error) {
+        throw new Error(`Erro ao inserir dados do endereço: ${error.message}`);
+    }
+};
+
 const updateEnderecoData = async (newEnderecoData) => {
-    const atualizarEnderecoQuery =   `
+    const atualizarEnderecoQuery = `
         UPDATE endereco
         SET cep = ?, estado = ?, cidade = ?, lodradouro = ?, 
         bairro = ?, numero = ?
@@ -25,5 +38,6 @@ const updateEnderecoData = async (newEnderecoData) => {
 
 module.exports = {
     getEnderecoById,
-    updateEnderecoData
+    updateEnderecoData,
+    createEndereco
 };
