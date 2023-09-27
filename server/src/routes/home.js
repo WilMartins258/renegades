1f1d3dd6bf3ec272812559f81e66563ef5a41008
WilMartins258 <willmartins258@gmail.com>
2023-09-27 11:07:07 -0300

    lapidando rota /home

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -1,31 +1,13 @@
-// Importando módulos
 const router = require('express').Router();
-const db = require('../models/db.js');
 
-
-// router.post('/', (req, res) => {
-//     console.log('passou pelo /home POST')
-//     res.send('Dados da página de home.');
-// });
-
-/**
- * Query que busca os dados so usuário com base no email enviado
- * @returns login_data
- */
-const getAllEstabelecimentos = async () => {
-    const connection = await db;
-    const estabelecimentoQuery = "SELECT * FROM estabelecimento";
-    const [estabelecimentos] = await connection.query(estabelecimentoQuery);
-
-    return estabelecimentos;
-}
+const home_Service = require('./../services/home.service.js');
 
 router.get('/', async (req, res) => {
-    const resultadoEstabelecimentos = await getAllEstabelecimentos();
-    console.log({resultadoEstabelecimentos});
+    const todosEstabelecimentos = await home_Service.pegarTudo();
+    console.log({todosEstabelecimentos});
 
     res.status(200).send({
-        resultadoEstabelecimentos
+        todosEstabelecimentos
     });   
     
 });
