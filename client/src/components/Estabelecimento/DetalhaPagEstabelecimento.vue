d4a12290640a0e42c64d8d898200adf8e7619c10
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-09-28 20:45:16 -0300

    Pagina Estabelecimento

--- client/src/components/Estabelecimento/DetalhaPagEstabelecimento.vue
+++ client/src/components/Estabelecimento/DetalhaPagEstabelecimento.vue
@@ -1,91 +1,91 @@
 <template>
-
-<header>
-        <h1>Nome do Estabelecimento</h1>
-        <p>Pizzaria</p>
-        <img src="imagem_do_estabelecimento.jpg" alt="Imagem do Estabelecimento">
-        <div class="icons">
-            <span class=" star-icon" :class="{ 'selected': favorito }" @click="toggleFavorito">&#9733;</span>
-            <span class="share-icon">Compartilhar</span>
-        </div>
+  <div>
+    <header>
+      <h1>{{ nomeDoEstabelecimento }}</h1>
+      <p>{{ categoria }}</p>
+      <img :src="imagemEstabelecimento" alt="Imagem do Estabelecimento" />
+      <div class="icons">
+        <span class="star-icon" :class="{ selected: favorito }" @click="toggleFavorito">&#9733;</span>
+        <span class="share-icon">Compartilhar</span>
+      </div>
     </header>
 
     <section class="description">
-        <h2>Descrição do Estabelecimento</h2>
-        <p>Coloque aqui a descrição do estabelecimento.</p>
+      <h2>Descrição do Estabelecimento</h2>
+      <p>{{ descricao }}</p>
     </section>
 
     <section class="chef-recomendacoes">
-        <!-- Quadros de recomendações do chef -->
-        <div class="recomendacao">
-            <h3>Prato Principal 1</h3>
-            <p>Descrição do prato principal 1.</p>
-            <img src="imagem_prato1.jpg" alt="Imagem do Prato Principal 1">
-        </div>
-        <div class="recomendacao">
-            <h3>Prato Principal 2</h3>
-            <p>Descrição do prato principal 2.</p>
-            <img src="imagem_prato2.jpg" alt="Imagem do Prato Principal 2">
-        </div>
-        <div class="recomendacao">
-            <h3>Prato Principal 3</h3>
-            <p>Descrição do prato principal 3.</p>
-            <img src="imagem_prato3.jpg" alt="Imagem do Prato Principal 3">
-        </div>
+      <h2>Recomendações do Chef</h2>
+      <div class="recomendacao" v-for="prato in pratosChef" :key="prato.id">
+        <h3>{{ prato.nome }}</h3>
+        <p>{{ prato.descricao }}</p>
+        <img :src="prato.imagem" :alt="'Imagem de ' + prato.nome" />
+      </div>
     </section>
 
     <section class="adicionais-info">
-        <!-- Quadros de informações adicionais -->
-        <div class="info">
-            <h3>O que oferecemos</h3>
-            <p>Estacionamento, área kids, etc.</p>
-        </div>
-        <div class="info">
-            <h3>Forma de Contato e Redes Sociais</h3>
-            <p>Email: exemplo@example.com<br>Telefone: (XX) XXXX-XXXX<br>Redes Sociais: <a href="#">Facebook</a>, <a href="#">Instagram</a></p>
-        </div>
-        <div class="info">
-            <h3>Horário de Atendimento</h3>
-            <p>Segunda a Sexta: 10:00 - 22:00<br>Sábado e Domingo: 12:00 - 23:00</p>
-        </div>
+      <div class="info">
+        <h3>O que oferecemos</h3>
+        <p>{{ opcional }}</p>
+      </div>
+      <div class="info">
+        <h3>Forma de Contato e Redes Sociais</h3>
+        <p>Email: {{ email }}</p>
+        <p>Telefone: {{ telefone }}</p>
+        <p>Redes Sociais: <a :href="facebook">{{ facebook }}</a>, <a :href="instagram">{{ instagram }}</a>, <a :href="twitter">{{ twitter }}</a></p>
+        <p>Nosso site: <a :href="site">{{ site }}</a></p>
+        <p>Cardápio Online: <a :href="cardapio">{{ cardapio }}</a></p>
+      </div>
+      <div class="info">
+        <h3>Horário de Atendimento</h3>
+        <p>Segunda a Sexta: 10:00 - 22:00</p>
+        <p>Sábado e Domingo: 12:00 - 23:00</p>
+      </div>
     </section>
-    
-<section class="endereco-info">
-    <div class="endereco">
-    <h2>Endereço</h2>
-    <p>Insira aqui o endereço do estabelecimento.</p>
-    </div>
-    <div class="endereco">
-    <!-- Espaço para exibir o mapa (substitua a URL pelo código do mapa real) -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.349676015265!2dSUA-LONGITUDE!3dSUA-LATITUDE!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zSsKwMTcnMzcuNSJTIDQwwrAyMSc1LjciRQ!5e0!3m2!1spt-BR!2sbr!4v1618986094141!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-    </div>
-</section>
 
-	
-    <section class="user-avaliacao">
-        <h2>Avalie</h2>
-        <form>
-            <label for="avaliacao">De uma Nota:</label>
-            <div class="rating">
-                <span class="star" :class="{ 'selected': nota >= 1 }" @click="selecionarNota(1)" >&#9733;</span>
-                <span class="star" :class="{ 'selected': nota >= 2 }" @click="selecionarNota(2)" >&#9733;</span>
-                <span class="star" :class="{ 'selected': nota >= 3 }" @click="selecionarNota(3)" >&#9733;</span>
-                <span class="star" :class="{ 'selected': nota >= 4 }" @click="selecionarNota(4)" >&#9733;</span>
-                <span class="star" :class="{ 'selected': nota >= 5 }" @click="selecionarNota(5)" >&#9733;</span>
-            </div>
-            <br>
-            <label for="user-review">Digite sua avaliação:</label>
-            <div class="group">
-            <textarea id="user-review" class="input" name="user-review" rows="4" cols="50" ref="userReview"></textarea>
-            <br>
-            
-            <div>
-                <input type="button" class="button" value="Avaliar" @click.prevent="avaliar"/>
-            </div>
-        </div>
-        </form>
+    <section class="endereco-info">
+      <div class="endereco">
+        <h2>Endereço</h2>
+        <p>{{ endereco }}</p>
+      </div>
+      <div class="endereco">
+        <!-- Replace with actual map integration -->
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.349676015265!2dSUA-LONGITUDE!3dSUA-LATITUDE!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zSsKwMTcnMzcuNSJTIDQwwrAyMSc1LjciRQ!5e0!3m2!1spt-BR!2sbr!4v1618986094141!5m2!1spt-BR!2sbr"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
     </section>
-     
+
+    <section class="user-avaliacao">
+      <h2>Avalie</h2>
+      <form>
+        <label for="avaliacao">Dê uma Nota:</label>
+        <div class="rating">
+          <span class="star" :class="{ selected: nota >= 1 }" @click="selecionarNota(1)">&#9733;</span>
+          <span class="star" :class="{ selected: nota >= 2 }" @click="selecionarNota(2)">&#9733;</span>
+          <span class="star" :class="{ selected: nota >= 3 }" @click="selecionarNota(3)">&#9733;</span>
+          <span class="star" :class="{ selected: nota >= 4 }" @click="selecionarNota(4)">&#9733;</span>
+          <span class="star" :class="{ selected: nota >= 5 }" @click="selecionarNota(5)">&#9733;</span>
+        </div>
+        <br />
+        <label for="user-review">Digite sua avaliação:</label>
+        <div class="group">
+          <textarea id="user-review" class="input" name="user-review" rows="4" cols="50" v-model="avaliacao"></textarea>
+          <br />
+
+          <div>
+            <input type="button" class="button" value="Avaliar" @click.prevent="avaliar" />
+          </div>
+        </div>
+      </form>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -95,6 +95,18 @@ export default {
         return {
             nota: 0, 
             favorito: false,
+            nomeDoEstabeleciment: "",
+            categoria: "",
+            imagemEstabelecimento: "",
+            opcional: "",
+            email: "",
+            telefone: "",
+            facebook: "",
+            instagram: "",
+            twitter: "",
+            site: "",
+            cardapio: "",
+            Endereco: "",
         };
     },
     methods: {
@@ -119,6 +131,21 @@ export default {
         toggleFavorito() {
             this.favorito = !this.favorito;
         },
+    },
+    mounted() {
+             
+             this.nomeDoEstabeleciment = "Jeff's Burger"; 
+             this.categoria = "Hamburgueria"; 
+             this.imagemEstabelecimento = ""; 
+             this.opcional = "Teste", 
+             this.email = "@teste"; 
+             this.telefone = "1533325151"; 
+             this.facebook = "@facebook"; 
+             this.instagram = "@instagram"; 
+             this.twitter = "@twitter"; 
+             this.site = "WWW"; 
+             this.cardapio = ""; 
+             this.endereco = "meu end.";
     }
 }
 
