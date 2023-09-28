26293dbbdfafdf15a1d6d3505f26089ff54f21d5
WilMartins258 <willmartins258@gmail.com>
2023-09-27 23:33:48 -0300

    testes na rota /home

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 const home_Service = require('./../services/home.service.js');
+const test_Service = require('./../services/test.service.js');
 
 router.get('/', async (req, res) => {
-    const todosEstabelecimentos = await home_Service.pegarTudo();
-    console.log({todosEstabelecimentos});
+    const tests = await test_Service.pegarTudo();
+    console.log('tests:: ', tests);
 
     res.status(200).send({
-        todosEstabelecimentos
-    });   
-    
+        test: 'testing 123'
+    });
 });
 
 module.exports = router;
