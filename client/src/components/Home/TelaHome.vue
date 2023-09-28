71649ab26c9e02b9b34ec5bc29ad17b123a574eb
WilMartins258 <willmartins258@gmail.com>
2023-09-27 23:33:29 -0300

    chamada para o método inicial na home

--- client/src/components/Home/TelaHome.vue
+++ client/src/components/Home/TelaHome.vue
@@ -1,31 +1,39 @@
 <template>
-        <h1>Better Choice!</h1>
-      <h3>Mais Recomendados!!</h3>
-      <CombinedCarousel/><br><br>
-      <Filtro/>
+  <h1>Better Choice!</h1>
+  <h3>Mais Recomendados!!</h3>
+  <CombinedCarousel /><br><br>
+  <Filtro />
+</template>
   
-  
-  </template>
-  
-  <script>
-  import CombinedCarousel from "../Home/ComponentsChild/Carousel.vue"
-  import Filtro from "../Home/ComponentsChild/Filtro.vue"
-      export default{
-          name: "TelaHome",
-          components:{
-            CombinedCarousel,
-            Filtro,
-          }
-      };
-  
-  </script>
+<script>
+import CombinedCarousel from "../Home/ComponentsChild/Carousel.vue";
+import Filtro from "../Home/ComponentsChild/Filtro.vue";
+import api from "./../../services/backend-service.js";
+
+export default {
+  name: "TelaHome",
+  components: {
+    CombinedCarousel,
+    Filtro,
+  },
+  created() {
+	  this.metodoInicial();
+  },
+  methods: {
+    async metodoInicial() {
+      console.log('metodoInicial da HOME');
+    }
+  }
+};
+
+</script>
 
 <style scoped>
 h1 {
   color: #fff;
 }
 
-h3{
-    color: #fff;
+h3 {
+  color: #fff;
 }
 </style>
