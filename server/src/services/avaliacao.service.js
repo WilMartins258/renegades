cb84fe376939e64057b7c89a74563c6d9238c894
WilMartins258 <willmartins258@gmail.com>
2023-10-02 19:54:26 -0300

    implementando query que recupera avaliações por idUsuario

--- server/src/services/avaliacao.service.js
+++ server/src/services/avaliacao.service.js
@@ -6,14 +6,18 @@ const atualizar = async () => {};
 
 const pegarPorIdUsuario = async (idUsuario) => {
     try {
-        const avaliacaoQuery = `SELECT idEstabelecimento from avaliacao WHERE idUsuario = ?;`;
+        const avaliacaoQuery = `
+        SELECT a.id, e.id as 'idEstabelecimento', e.nome, a.descricao, a.nota
+            FROM  estabelecimento e JOIN avaliacao a
+            on e.id = a.idEstabelecimento
+                WHERE idUsuario = ?;`;
         const connection = await db;
 
-        const [avaliacao] = await connection.query(avaliacaoQuery, idUsuario);
+        const [avaliacoes] = await connection.query(avaliacaoQuery, idUsuario);
 
-        return avaliacao;
+        return avaliacoes;
     } catch (error) {
-        console.error('ERROR:: ', error);
+        throw new Error(`Erro ao buscar avaliações do usuário: ${error.message}`);
     }
 };
 
