5fd90d141b7b64049b37337e0f50de33f35b92be
WilMartins258 <willmartins258@gmail.com>
2023-10-02 20:11:45 -0300

    acertando rota get das avaliações

--- server/src/routes/avaliacao.js
+++ server/src/routes/avaliacao.js
@@ -4,14 +4,14 @@ const avaliacao_Service = require('../services/avaliacao.service.js');
 router.get('/:idUsuario', async (req, res) => {
     try {
         const idUsuario = req?.params?.idUsuario;
-        console.log('idUsuario:: ', idUsuario);
-        
         const avaliacao = await avaliacao_Service.pegarPorIdUsuario(idUsuario);
-        console.log('avaliacao:: ', avaliacao);
 
         res.status(200).send(avaliacao);
     } catch (error) {
         console.error('ERROR:: ', error);
+        res.status(400).send({
+            msg: error
+        });
     }
 });
 
