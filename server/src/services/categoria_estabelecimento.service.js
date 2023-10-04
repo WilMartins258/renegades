266f809c221139c3cd96764c08b3738d33bc9649
WilMartins258 <willmartins258@gmail.com>
2023-10-03 22:00:47 -0300

    iniciando método pegarPorIdEstabelecimento de categoria_estabelecimento

--- server/src/services/categoria_estabelecimento.service.js
+++ server/src/services/categoria_estabelecimento.service.js
@@ -12,13 +12,23 @@ const inserir = async (dadosCategoria, conn) => {
 
 const atualizar = async () => {};
 
-const pegarPorId = async () => {};
+const pegarPorIdEstabelecimento = async (idEstabelecimento, conn) => {
+    try {
+        const categoriaEstabelecimentoQuery = '';
+
+        const [idCategoriaEstabelecimento] = await conn.query(categoriaEstabelecimentoQuery, idEstabelecimento);
+
+        return idCategoriaEstabelecimento;
+    } catch (error) {
+        throw new Error(`Erro ao inserir categoria do estabelecimento: ${error.message}`);
+    }
+};
 
 const excluir = async () => {};
 
 module.exports = {
     inserir,
     atualizar,
-    pegarPorId,
+    pegarPorIdEstabelecimento,
     excluir
 };
