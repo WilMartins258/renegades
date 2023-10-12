9764fa6653af86e0cb9f55005e4ae37cb351fd43
WilMartins258 <willmartins258@gmail.com>
2023-10-12 18:03:03 -0300

    rota GET do carousel

--- server/src/routes/carousel.js
+++ server/src/routes/carousel.js
@@ -4,7 +4,16 @@ const router = express.Router();
 const estabelecimento_Service = require('./../services/estabelecimento.service.js');
 
 router.get('/', async (req, res) => {
+    try {
+        const estabelecimentosCarousel = await estabelecimento_Service.carousel();
 
+        res.status(200).send(estabelecimentosCarousel);  
+    } catch (error) {
+        res.status(500).send({
+            errorMsg: 'Erro ao buscar estabelecimentos',
+            error: error.message
+        });
+    }
 });
 
 module.exports = router;
