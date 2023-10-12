acf61fb7f376feddbed7d45d8be7bb73461ca1ce
WilMartins258 <willmartins258@gmail.com>
2023-10-12 17:01:13 -0300

    declaração da rota do carousel

---
+++ server/src/routes/carousel.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const estabelecimento_Service = require('./../services/estabelecimento.service.js');
+
+router.get('/', async (req, res) => {
+
+});
+
+module.exports = router;
