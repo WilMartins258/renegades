f30c848a85c378d21a3be578afb1674e0fbf1fc7
WilMartins258 <willmartins258@gmail.com>
2023-10-12 23:46:17 -0300

    corrigindo rota de filtro

--- server/src/routes/filtro.js
+++ server/src/routes/filtro.js
@@ -8,17 +8,17 @@ const comida_Service = require('../services/comida.service.js');
 
 router.get('/', async (req, res) => {
     try {
-        const estabelecimentosFiltro = await estabelecimento_Service.filtros();
+        const estabelecimentos = await estabelecimento_Service.filtros();
         const categorias = await categoria_Service.pegarTudo();
         const opcionais = await opcionaol_Service.pegarTudo();
         const comidas = await comida_Service.pegarTudo();
 
-        res.status(200).send(
-            estabelecimentosFiltro,
+        res.status(200).send({
+            estabelecimentos,
             categorias,
             opcionais,
             comidas
-        );  
+        });  
     } catch (error) {
         res.status(500).send({
             errorMsg: 'Erro ao buscar estabelecimentos',
