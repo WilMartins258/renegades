b604e07dc102956bfc7c1fd1188572a5b28a1b4e
WilMartins258 <willmartins258@gmail.com>
2023-10-12 23:40:10 -0300

    rota dos filtros

--- server/src/routes/filtro.js
+++ server/src/routes/filtro.js
@@ -2,12 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const estabelecimento_Service = require('./../services/estabelecimento.service.js');
+const categoria_Service = require('../services/categoria.service.js');
+const opcionaol_Service = require('../services/opcional.service.js');
+const comida_Service = require('../services/comida.service.js');
 
 router.get('/', async (req, res) => {
     try {
         const estabelecimentosFiltro = await estabelecimento_Service.filtros();
+        const categorias = await categoria_Service.pegarTudo();
+        const opcionais = await opcionaol_Service.pegarTudo();
+        const comidas = await comida_Service.pegarTudo();
 
-        res.status(200).send(estabelecimentosFiltro);  
+        res.status(200).send(
+            estabelecimentosFiltro,
+            categorias,
+            opcionais,
+            comidas
+        );  
     } catch (error) {
         res.status(500).send({
             errorMsg: 'Erro ao buscar estabelecimentos',
