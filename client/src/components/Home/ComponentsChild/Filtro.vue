a972597b3c431a485c7a778d9b198be3049daa3c
WilMartins258 <willmartins258@gmail.com>
2023-10-14 14:33:01 -0300

    continuando tratamento de estabelecimentos para estabelecimentos filtro

--- client/src/components/Home/ComponentsChild/Filtro.vue
+++ client/src/components/Home/ComponentsChild/Filtro.vue
@@ -130,7 +130,7 @@
           class="card-image"
         />
         <h3>{{ estabelecimento.nome }}</h3>
-        <p class="description">{{ estabelecimento.categoria }}</p>
+        <p class="description">{{ estabelecimento.categoriasString }}</p> <!--Era estabelecimento.categoria-->
       </div>
     </a>
   </div>
@@ -218,13 +218,83 @@ export default {
 
       try {
         for (let i=0; i < estabelecimentos.length; i++) {
-          console.log('i:: ', i)
           estabelecimentos[i].foto = require(`./../../Estabelecimento/images/${estabelecimentos[i].id}.${estabelecimentos[i].formatoFoto}`);
         };
-        this.estabelecimentos = estabelecimentos;
       } catch (error) {
         console.log('Erro ao tratar estabelecimentos para filtro da home: ', error);
       }
+      // console.log('estabelecimentos:: ', estabelecimentos);
+      // this.estabelecimentos = estabelecimentos;
+      
+      const estabelecimentosTeste = [
+        {
+          id: 1,
+          nome: "Jeff's Burger",
+          categoria: ["Lanchonete", "Pizzaria", "Restaurante"].join(', '),
+          foto: "https://www.plakart.com.br/img/galerias/40/0004_19b5c1b5b20643f9fc9045e14cd8ef67.jpeg",
+          opcionais: ["Wifi", "Precisa de Agendamento", "Área Kids"],
+          comidas: [null]
+        },
+        {
+          id: 2,
+          nome: "Bar do João",
+          categoria: ["Bar"].join(', '),
+          foto: "https://www.lojaskd.com.br/blog/wp-content/webp-express/webp-images/doc-root/blog/wp-content/uploads/2023/06/Nomes-para-bar-1024x576.png.webp",
+          opcionais: ["Wifi", "Estacionamento",],
+          comidas: [null]
+        },
+        {
+          id: 3,
+          nome: "Lanchonete da Maria",
+          categoria: ["Lanchonete", "Pizzaria"].join(', '),
+          foto: "https://www.emporiotambo.com.br/pub/media/resized/1300x800/ves/blog/xcomo-decorar-uma-lanchonete-com-pouco-dinheiro.jpg.pagespeed.ic.o-02P9_HPT.webp",
+          opcionais: ["Estacionamento", "Área Kids"],
+          comidas: [null]
+        },
+        {
+          id: 4,
+          nome: "Pizzaria do Carlos",
+          categoria: ["Pizzaria"].join(', '),
+          foto: "https://diariodorio.com/wp-content/uploads/2020/07/daleopizzaria_20200710_144435_0.jpg",
+          opcionais: ["Wifi", "Área Kids"],
+          comidas: [null]
+        },
+        {
+          id: 5,
+          nome: "Burger King",
+          categoria: ["Hamburgueria"].join(', '),
+          foto: "https://agendasorocaba.com.br/wp-content/uploads/2019/07/Burger-King-03.jpg",
+          opcionais: ["Wifi", "Couvert Grátis", "Área Kids"],
+          comidas: [null]
+        },
+        {
+          id: 6,
+          nome: "Izumi",
+          categoria: ["Restaurante"].join(', '),
+          foto: "https://izumima.com/wp-content/uploads/2023/01/IMG_8822-2048x1536.jpg",
+          opcionais: ["Wifi", "Possui Área de Fumantes",],
+          comidas: ["Japonesa"]
+        },
+        {
+          id: 7,
+          nome: "Absoluto",
+          categoria: ["Restaurante"].join(', '),
+          foto: "https://i.pinimg.com/564x/49/0b/64/490b640ca8bb4726489296c98509fdb6.jpg",
+          opcionais: ["Wifi", "Permite Animais", "Área Kids", "Estacionamento"],
+          comidas: ["Italiana"]
+        },
+        {
+          id: 8,
+          nome: "Bumbu",
+          categoria: ["Restaurante"].join(', '),
+          foto: "https://4.bp.blogspot.com/-JiIZTUI6GzY/WN1phr5KOYI/AAAAAAAAANc/wALXGWFUwCo-ZK6pGrsPJ0UlyCpbTMqLQCLcB/s1600/fachada-bambu.jpg",
+          opcionais: ["Wifi", "Área Kids"],
+          comidas: ["Brasileira"]
+        }
+      ]
+
+      this.estabelecimentos = estabelecimentosTeste;
+
 
       try {
         const opcionaisArray = opcionais.map(objeto => objeto.nome);
