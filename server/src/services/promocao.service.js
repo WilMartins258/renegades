afed69eff7ac7ccdf4b48b70cced0aecf8bdfbcc
WilMartins258 <willmartins258@gmail.com>
2023-10-16 20:29:06 -0300

    service de promoções

---
+++ server/src/services/promocao.service.js
@@ -0,0 +1,32 @@
+const db = require('../models/db.js');
+
+const inserir = async (dadosPromocao) => {
+    try {
+        const promocaoQuery = `INSERT into opcional (nome, ativo) VALUES (?, ?);`;
+        const connection = await db;
+
+        const [insercaoPromocao] = await connection.query(promocaoQuery, dadosPromocao);
+
+        return insercaoPromocao?.insertId;
+    } catch (error) {
+        throw new Error(`Erro ao inserir promocao: ${error.message}`);
+    }
+};
+
+const pegarPromocoesAtivas = async () => {
+    try {
+        const promocaoQuery = `SELECT * FROM promocao;`;
+        const connection = await db;
+
+        const [promocoesAtivas] = await connection.query(promocaoQuery);
+
+        return promocoesAtivas;
+    } catch (error) {
+        throw new Error(`Erro ao buscar promocoes: ${error.message}`);
+    }
+}
+
+module.exports = {
+    inserir,
+    pegarPromocoesAtivas
+};
