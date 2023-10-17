44ac3866972af0fc62b25b290297ed1fb5ad1353
WilMartins258 <willmartins258@gmail.com>
2023-10-17 12:09:04 -0300

    tratando categorias dos estabelecimentos

--- server/src/routes/promocao.js
+++ server/src/routes/promocao.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const promocao_Service = require('../services/promocao.service.js');
+const categoria_Service = require('../services/categoria.service.js');
 
 router.get('/', async (req, res) => {
     try {
         const promocoesAtivas = await promocao_Service.pegarPromocoesAtivas();
+        const categorias = await categoria_Service.pegarTudo();
 
-        res.status(200).send(promocoesAtivas);
+        for (let i=0; i < promocoesAtivas.length; i++) {
+            if (promocoesAtivas[i].categoriasString) {
+                promocoesAtivas[i].categoria = promocoesAtivas[i].categoriasString.split(',').map(item => item.trim());
+            } else {
+                promocoesAtivas[i].categoria = [null];
+            }
+        }
+
+        res.status(200).send({
+            promocoesAtivas,
+            categorias
+        });
     } catch (error) {
         console.log('ERROR:: ', error);
         res.status(500).send({
