e7ee8270f6c57ad52649c7dd61cf8ff2562c99ec
WilMartins258 <willmartins258@gmail.com>
2023-10-17 11:33:35 -0300

    chamda para o serviço de verificar promoções

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -4,7 +4,7 @@ const promocao_Service = require('../services/promocao.service.js');
 
 router.get('/', async (req, res) => {
     try {
-
+        await promocao_Service.verificarValidadeDePromocoes();
 
         res.status(200).send();
     } catch (error) {
