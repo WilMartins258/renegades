1da873f41fe9c11d4575131877eccf77580675c1
LucasMaxSantos <78882506+LucasMaxSantos@users.noreply.github.com>
2023-10-17 21:15:47 -0300

    Dados fake lista de Estabelecimento

--- client/src/components/Adm/ListaEstabelecimento.vue
+++ client/src/components/Adm/ListaEstabelecimento.vue
@@ -86,23 +86,23 @@ export default {
   },
   async created() {
     try {
-      // const estabelecimentosRequest = await api.get('/estabelecimento/validacao');
+      const estabelecimentosRequest = await api.get('/estabelecimento/validacao');
 
-      // for (let i=0; i < estabelecimentosRequest.data.length ; i++) {
-      //   estabelecimentosRequest.data[i].link = `http://localhost:8080/PaginaEstabelecimento/${estabelecimentosRequest.data[i].id}`;
-      // };
+      for (let i=0; i < estabelecimentosRequest.data.length ; i++) {
+        estabelecimentosRequest.data[i].link = `http://localhost:8080/PaginaEstabelecimento/${estabelecimentosRequest.data[i].id}`;
+      };
 
-      // console.log('estabelecimentosRequest.data:: ', estabelecimentosRequest.data);
+      console.log('estabelecimentosRequest.data:: ', estabelecimentosRequest.data);
 
-      // this.estabelecimentos = estabelecimentosRequest.data;
-      // this.estabelecimentos = [
-      //     { id: 1, nome: "Estabelecimento X", link: "/atulestabelecimento", status: "Pendente" },
-      //     { id: 2, nome: "Estabelecimento Y", link: "/atulestabelecimento", status: "Validado" },
-      //     { id: 3, nome: "Estabelecimento Z", link: "/atulestabelecimento", status: "Não Validado" },
-      //     { id: 4, nome: "Estabelecimento G", link: "/atulestabelecimento", status: "Pendente" },
-      //     { id: 5, nome: "Estabelecimento H", link: "/atulestabelecimento", status: "Validado" },
-      //     { id: 6, nome: "Estabelecimento P", link: "/atulestabelecimento", status: "Não Validado" },
-      //   ];
+      this.estabelecimentos = estabelecimentosRequest.data;
+      this.estabelecimentos = [
+          { id: 1, nome: "Estabelecimento X", link: "/atulestabelecimento", status: "Pendente" },
+          { id: 2, nome: "Estabelecimento Y", link: "/atulestabelecimento", status: "Validado" },
+          { id: 3, nome: "Estabelecimento Z", link: "/atulestabelecimento", status: "Não Validado" },
+          { id: 4, nome: "Estabelecimento G", link: "/atulestabelecimento", status: "Pendente" },
+          { id: 5, nome: "Estabelecimento H", link: "/atulestabelecimento", status: "Validado" },
+          { id: 6, nome: "Estabelecimento P", link: "/atulestabelecimento", status: "Não Validado" },
+        ];
       
     } catch (error) {
       console.log('Erro ao buscar estabelecimentos para validação: ', error);
@@ -157,7 +157,7 @@ export default {
   }
   
   .table-container {
-    max-height: 300px; /* Ajuste a altura conforme necessário */
+ 
     overflow: auto;
   }
 
