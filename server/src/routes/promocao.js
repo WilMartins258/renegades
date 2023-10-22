a92bf0c67947765f9cf3736a3292b9506ac4abce
WilMartins258 <willmartins258@gmail.com>
2023-10-21 23:18:50 -0300

    rota para buscar promoções do estabelecimento

--- server/src/routes/promocao.js
+++ server/src/routes/promocao.js
@@ -29,9 +29,28 @@ router.get('/usuario', async (req, res) => {
     }
 });
 
-router.get('/estabelecimento', async (req, res) => {
+router.get('/estabelecimento/:idEstabelecimento', async (req, res) => {
     try {
+        const todasPromocoes = await promocao_Service.pegarPorIdEstabelecimento(req.params.idEstabelecimento);
 
+        const promocoesAtivas = []
+        const promocoesInativas = []
+        const promocoesVencidas = []
+        for (let i=0; i < todasPromocoes.length; i++) {
+            if (todasPromocoes[i].status == 'Ativa') {
+                promocoesAtivas.push(todasPromocoes[i]);
+            } else if (todasPromocoes[i].status == 'Inativa') {
+                promocoesInativas.push(todasPromocoes[i]);
+            } else {
+                promocoesVencidas.push(todasPromocoes[i]);
+            }
+        }
+
+        res.status(200).send({
+            promocoesAtivas,
+            promocoesInativas,
+            promocoesVencidas
+        });
     } catch (error) {
         console.log('ERROR:: ', error);
         res.status(500).send({
