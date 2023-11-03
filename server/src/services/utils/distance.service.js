06e278bc7450ec921dca16846f8d08e9fa7fa4c8
WilMartins258 <willmartins258@gmail.com>
2023-11-03 15:21:40 -0300

    obterCoordenadasViaGoogleApi

--- server/src/services/utils/distance.service.js
+++ server/src/services/utils/distance.service.js
@@ -1,1 +1,27 @@
 const axios = require('axios');
+const variaveisGlobais = require('./../../../variaveis.js');
+
+const calcularDistancia = async (lat1, lon1, lat2, lon2) => {
+    const apiUrl = 'http://localhost:3001/distancia';
+    const response = await axios.get(apiUrl);
+    console.log('response:: ', response);
+    return response;
+};
+
+const obterCoordenadasViaGoogleApi = async (numeroResidencial, cep) => {
+    console.log('obterCoordenadasViaGoogleApi');
+    const apiKey = variaveisGlobais.googleApiKey;
+    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${numeroResidencial},${cep}&key=${apiKey}`;
+    
+    try {
+        const response = await axios.get(apiUrl);
+        console.log('response.data');
+        return response.data;
+    } catch (error) {
+        throw new Error('Erro ao fazer a requisição para a API do Google Maps Geocoding: ', error);
+    }
+};
+
+module.exports = {
+    obterCoordenadasViaGoogleApi
+} 
