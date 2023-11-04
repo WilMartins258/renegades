652ef1abeefe352af59cebd5a311a6998eb0615c
WilMartins258 <willmartins258@gmail.com>
2023-11-04 13:29:34 -0300

    transformando distância de metros para kilometros

--- server/src/routes/estabelecimento.js
+++ server/src/routes/estabelecimento.js
@@ -86,9 +86,9 @@ router.get('/filtro', async (req, res) => {
 
                 if (latitude && longitude) {
                     if (estabelecimentos[i].cep) {
-                        estabelecimentos[i].distancia = await distancia_Service.obterDistanciaDoEstabelecimento(latitude, longitude, estabelecimentos[i].numeroEstabelecimento, estabelecimentos[i].cep);
+                        estabelecimentos[i].distancia = (await distancia_Service.obterDistanciaDoEstabelecimento(latitude, longitude, estabelecimentos[i].numeroEstabelecimento, estabelecimentos[i].cep)/1000);
                     } else if (estabelecimentos[i].latitude && estabelecimentos[i].longitude) {
-                        estabelecimentos[i].distancia = await distancia_Service.obterDistanciaDoEstabelecimento(latitude, longitude, estabelecimentos[i].latitude, estabelecimentos[i].longitude);
+                        estabelecimentos[i].distancia = (await distancia_Service.obterDistanciaDoEstabelecimento(latitude, longitude, estabelecimentos[i].latitude, estabelecimentos[i].longitude)/1000);
                     } else {
                         estabelecimentos[i].distancia = null;
                     }
