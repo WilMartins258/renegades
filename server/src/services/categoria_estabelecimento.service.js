220f97db2ceaa6d243455e3434cdeb4b72616030
WilMartins258 <willmartins258@gmail.com>
2023-11-08 22:07:41 -0300

    add idCategoriaEstabelecimento

--- server/src/services/categoria_estabelecimento.service.js
+++ server/src/services/categoria_estabelecimento.service.js
@@ -17,7 +17,7 @@ const atualizar = async () => {};
 const pegarPorIdEstabelecimento = async (idEstabelecimento) => {
     try {
         const categoriaEstabelecimentoQuery = `
-        SELECT cat.id, cat.nome
+        SELECT cat.id, cat.nome, ce.id as 'idCategoriaEstabelecimento'
             FROM categoria_estabelecimento ce
             JOIN categoria cat ON ce.idCategoria = cat.id
                 WHERE ce.idEstabelecimento = ?;`;
