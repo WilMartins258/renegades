310c9f4a6d127d6a955c245bdb09947fcc2a68bc
WilMartins258 <willmartins258@gmail.com>
2023-11-18 15:49:20 -0300

    pegar status no login

--- server/src/services/login.service.js
+++ server/src/services/login.service.js
@@ -4,7 +4,7 @@ const db = require('../models/db.js');
  * Query que busca os dados so usuário com base no email enviado
  */
 const fazerLogin = async (emailDoUsuario) => {
-    const loginQuery = `SELECT id, nome, idEstabelecimento, email, senha, fotoperfil, tipoUsuario 
+    const loginQuery = `SELECT id, nome, idEstabelecimento, email, senha, fotoperfil, tipoUsuario, status
                         FROM usuario 
                         WHERE email = ?;`;
     const connection = await db;
